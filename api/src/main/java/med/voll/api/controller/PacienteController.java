3f6c8a24fbf6b21429425292c0eb8db5aa3bdf5c
Elvis de Oliveira <elvismuse@gmail.com>
2023-06-01 21:47:02 -0300

    tratando erro 404

--- api/src/main/java/med/voll/api/controller/PacienteController.java
+++ api/src/main/java/med/voll/api/controller/PacienteController.java
@@ -1,8 +1,7 @@
 package med.voll.api.controller;
 
 import jakarta.validation.Valid;
-import med.voll.api.medico.DadosAtualizacaoMedico;
-import med.voll.api.medico.DadosListagemMedico;
+import med.voll.api.domain.paciente.*;
 import med.voll.api.paciente.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
